b34c8118fb9f810cdb11aac54e62070429a2c780
gordonmhy <gh@hpri.33mail.com>
2021-10-13 18:59:24 +0800

    Completed launching page

--- App.js
+++ App.js
@@ -1,18 +1,74 @@
-import React from 'react';
-import {StyleSheet, Text, View, SafeAreaView} from 'react-native';
+import React, {useState} from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+    TextInput,
+    KeyboardAvoidingView,
+    TouchableOpacity,
+    SafeAreaView,
+    Linking, Alert
+} from 'react-native';
 
 export default function App() {
+
+    // Stores input credentials ready to be submitted
+    const [credentials, setCredentials] = useState({
+        username: '',
+        password: '',
+    })
+
+    // Prompts alert when user presses 'Register'
+    const confirmExternalRegistration = () => {
+        Alert.alert(
+            'Registration',
+            'This will open your browser and you will be navigated to the online registration page.',
+            [
+                {text: 'Back'},
+                {
+                    text: 'Proceed', onPress: () => {
+                        Linking.openURL('https://www.gordonmhy.com/tutorial/register/')
+                    }
+                }
+            ])
+    }
+
     return (
         <View style={styles.container}>
-
-            {/* Top title bar */}
-            <View style={styles.titleContainer}>
-                <SafeAreaView>
-                    <Text style={styles.titleText}>Tutorial Portal</Text>
+            <Text style={styles.titleText}>Tutorial Portal</Text>
+            <KeyboardAvoidingView style={styles.loginContainer}>
+                <Text style={styles.loginTitle}>Getting Started</Text>
+                {/* User Input Fields */}
+                <SafeAreaView style={styles.inputFieldContainer}>
+                    <TextInput style={[styles.inputField, styles.usernameInput]}
+                               placeholder={'Enter your username'}
+                               value={credentials.username}
+                               onChangeText={(username) => setCredentials({
+                                   ...credentials,
+                                   username: username
+                               })}/>
                 </SafeAreaView>
-            </View>
-
-
+                <SafeAreaView style={styles.inputFieldContainer}>
+                    <TextInput style={[styles.inputField, styles.passwordInput]}
+                               placeholder={'Enter your password'}
+                               secureTextEntry={true}
+                               value={credentials.password}
+                               onChangeText={(password) => setCredentials({
+                                   ...credentials,
+                                   password: password
+                               })}/>
+                </SafeAreaView>
+                {/* Buttons */}
+                <View style={styles.loginButtonsContainer}>
+                    <TouchableOpacity style={styles.buttons}
+                                      onPress={confirmExternalRegistration}>
+                        <Text style={styles.buttonText}>Register</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.buttons}>
+                        <Text style={styles.buttonText}>Login</Text>
+                    </TouchableOpacity>
+                </View>
+            </KeyboardAvoidingView>
         </View>
     );
 }
@@ -21,19 +77,61 @@ const styles = StyleSheet.create({
     // Main Container
     container: {
         flex: 1,
-        flexDirection: "column",
-        backgroundColor: 'white',
+        backgroundColor: '#fbefeb',
+        alignItems: 'center',
+        justifyContent: 'center',
     },
 
     // Title
-    titleContainer: {
-        backgroundColor: '#fbefeb',
-        alignItems: 'center',
-    },
     titleText: {
-        paddingVertical: 10,
-        fontSize: 25,
-    }
+        marginBottom: 20,
+        fontSize: 35,
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
 
+    // Login Panel
+    loginTitle: {
+        marginBottom: 10,
+        fontSize: 25,
+        textAlign: 'center',
+    },
+    loginContainer: {
+        backgroundColor: 'white',
+        paddingHorizontal: 20,
+        paddingVertical: 10,
+        borderRadius: 20,
+        width: 300,
+    },
+    inputFieldContainer: {
+        borderRadius: 5,
+        borderStyle: 'solid',
+        borderColor: 'lightgray',
+        borderWidth: 1,
+        padding: 100,
+        marginVertical: 5,
+    },
+    inputField: {
+        fontSize: 20,
+        textAlign: "center",
+    },
+    usernameInput: {},
+    passwordInput: {},
+    loginButtonsContainer: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+    },
+    buttons: {
+        borderRadius: 5,
+        backgroundColor: 'black',
+        padding: 7,
+        marginTop: 10,
+        marginHorizontal: 5,
+    },
+    buttonText: {
+        fontWeight: 'bold',
+        color: 'white',
+        fontSize: 17,
+    }
 
 });
